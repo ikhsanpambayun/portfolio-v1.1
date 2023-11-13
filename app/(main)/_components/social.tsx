68d530ba59d9107fbbed2ba9bac6b54f8cb824aa
Muhammad Ikhsan Asa Pambayun <ikhsan.pambayun@gmail.com>
2023-11-13 23:12:20 +0700

    change on click follow me

--- app/(main)/_components/social.tsx
+++ app/(main)/_components/social.tsx
@@ -1,7 +1,6 @@
 import { LucideIcon } from "lucide-react";
 import { motion } from "framer-motion";
 import { useState } from "react";
-import Link from "next/link";
 
 interface SocialProps {
   logo: LucideIcon;
@@ -19,48 +18,51 @@ const Social = ({ logo: Logo, url }: SocialProps) => {
     setAnimateLogo(false);
   };
 
+  const handleClickVisit = (site: string) => {
+    window.open(site, "_blank");
+  };
+
   return (
-    <Link href={url}>
-      <div
-        className="w-[70px] sm:w-[100px] hover:w-[130px] aspect-square bg-muted-foreground/20 backdrop-blur-md hover:bg-gradient-to-br hover: hover:from-muted-foreground/5 hover:to-primary/20 rounded-lg transition-all duration-300 overflow-hidden"
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
+    <div
+      className="w-[70px] sm:w-[100px] hover:w-[130px] aspect-square bg-muted-foreground/20 backdrop-blur-md hover:bg-gradient-to-br hover: hover:from-muted-foreground/5 hover:to-primary/20 rounded-lg transition-all duration-300 overflow-hidden cursor-pointer"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onClick={() => handleClickVisit(url)}
+    >
+      <motion.div
+        initial={{
+          y: 0,
+        }}
+        animate={
+          animateLogo
+            ? {
+                y: -100,
+                opacity: 0,
+              }
+            : "initial"
+        }
+        className="flex justify-center items-center w-full h-full"
       >
-        <motion.div
-          initial={{
-            y: 0,
-          }}
-          animate={
-            animateLogo
-              ? {
-                  y: -100,
-                  opacity: 0,
-                }
-              : "initial"
-          }
-          className="flex justify-center items-center w-full h-full"
-        >
-          <Logo className="w-[70%] h-[70%]" strokeWidth={1.5} />
-        </motion.div>
-        <motion.div
-          initial={{
-            y: 100,
-            opacity: 0,
-          }}
-          animate={
-            animateLogo
-              ? {
-                  y: 0,
-                  opacity: 1,
-                }
-              : "initial"
-          }
-          className="absolute top-0 flex justify-center items-center w-full h-full"
-        >
-          <Logo className="w-[70%] h-[70%]" strokeWidth={1.5} />
-        </motion.div>
-      </div>
-    </Link>
+        <Logo className="w-[70%] h-[70%]" strokeWidth={1.5} />
+      </motion.div>
+      <motion.div
+        initial={{
+          y: 100,
+          opacity: 0,
+        }}
+        animate={
+          animateLogo
+            ? {
+                y: 0,
+                opacity: 1,
+              }
+            : "initial"
+        }
+        className="absolute top-0 flex justify-center items-center w-full h-full"
+      >
+        <Logo className="w-[70%] h-[70%]" strokeWidth={1.5} />
+      </motion.div>
+    </div>
   );
 };
 
